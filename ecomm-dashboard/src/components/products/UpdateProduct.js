d231c45d64b76dbd9b3a783d79a9c77ddff67f40
Sharif Ahmed <sharif@namespaceit.com>
2021-04-01 17:23:57 +0600

    integrated update product api in frontend

--- ecomm-dashboard/src/components/products/UpdateProduct.js
+++ ecomm-dashboard/src/components/products/UpdateProduct.js
@@ -5,11 +5,40 @@ import {withRouter} from 'react-router-dom'
 const UpdateProduct = (props) => {
 
     const [data, setData] = useState([])
-    useEffect( async () => {
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState('')
+    const [description, setDescription] = useState('')
+    const [file, setFile] = useState('')
+
+    useEffect(  () => {
+        getData()
+    },[])
+
+    async function editProduct(id) {
+
+        let formData = new FormData()
+        formData.append('name', name)
+        formData.append('price', price)
+        formData.append('description', description)
+        formData.append('file_path', file)
+
+        let result = await fetch('http://127.0.0.1:8000/api/updateproduct/'+id+'?_method=PUT', {
+            method: 'POST',
+            body: formData
+        })
+        alert('product updated')
+        getData()
+    }
+
+    async function getData() {
         let result = await fetch('http://127.0.0.1:8000/api/product/'+props.match.params.id)
         result = await result.json()
         setData(result)
-    },[])
+        setName(result.name)
+        setPrice(result.price)
+        setDescription(result.description)
+        setFile(result.file)
+    }
 
     return (
         <div>
@@ -17,17 +46,17 @@ const UpdateProduct = (props) => {
             <div className="col-sm-6 offset-sm-3">
                 <h1>Update Product</h1>
                 <br/>
-                <input type="text" defaultValue={data.name} placeholder="Enter Product Name" className="form-control"/>
+                <input type="text" defaultValue={data.name} onChange={(e) => setName(e.target.value)} placeholder="Enter Product Name" className="form-control"/>
                 <br/>
-                <input type="text" defaultValue={data.price} placeholder="Enter Product Price" className="form-control"/>
+                <input type="text" defaultValue={data.price} onChange={(e) => setPrice(e.target.value)} placeholder="Enter Product Price" className="form-control"/>
                 <br/>
-                <input type="text" defaultValue={data.description} placeholder="Enter Product Description" className="form-control"/>
+                <input type="text" defaultValue={data.description} onChange={(e) => setDescription(e.target.value)} placeholder="Enter Product Description" className="form-control"/>
                 <br/>
-                <input type="file" defaultValue={data.file_path}  className="form-control"/>
+                <input type="file" defaultValue={data.file_path} onChange={(e) => setFile(e.target.files[0])}  className="form-control"/>
                 <br/>
                 <img width='80' height='80' src={'http://127.0.0.1:8000/'+data.file_path} />
                 <br /> <br />
-                <button className="btn btn-primary">Update Product</button>
+                <button className="btn btn-primary" onClick={() => editProduct(data.id)}>Update Product</button>
             </div>
         </div>
     );
