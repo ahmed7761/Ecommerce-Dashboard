81a199dbf19b9609a93b954bd9c9fd5ed04bf787
Sharif Ahmed <sharif@namespaceit.com>
2021-04-01 15:07:19 +0600

    integrate delete product api in frontend

--- ecomm-dashboard/src/components/products/ProductList.js
+++ ecomm-dashboard/src/components/products/ProductList.js
@@ -5,11 +5,25 @@ import {Table} from "react-bootstrap";
 const ProductList = () => {
 
     const [data, setData] = useState([])
-    useEffect(async () => {
+    useEffect( () => {
+        getData()
+    },[])
+
+    async function deleteOperation(id) {
+        let result = await fetch('http://127.0.0.1:8000/api/productdelete/'+id, {
+            method: 'DELETE'
+        })
+        result = await result.json()
+        console.log(result)
+        getData()
+    }
+
+    async function getData() {
         let result = await fetch('http://127.0.0.1:8000/api/productlist')
         result = await result.json()
         setData(result)
-    })
+    }
+
     return (
         <div>
             <Header />
@@ -23,6 +37,7 @@ const ProductList = () => {
                         <th>Image</th>
                         <th>Description</th>
                         <th>Price</th>
+                        <th>Action</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -34,6 +49,7 @@ const ProductList = () => {
                                 <td><img width='80' height='80' src={'http://127.0.0.1:8000/'+item.file_path} /></td>
                                 <td>{item.description}</td>
                                 <td>{item.price}</td>
+                                <td><button className='btn btn-primary btn-sm' onClick={() => deleteOperation(item.id)}>Delete</button></td>
                             </tr>
                         )
                     }
